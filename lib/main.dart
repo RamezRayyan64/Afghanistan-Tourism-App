3d8fa6229de61e4775f1e8935f3ad1f39eff9094
RamezRayyan64 <Ramez.Rayyan64@gmail.com>
2023-05-31 14:43:36 +0430

    splash screen created

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:afghanistan_tourism_app/packages/theme/light_theme.dart';
 import 'package:afghanistan_tourism_app/screens/splash_screen/splash_screen.dart';
 import 'package:afghanistan_tourism_app/utilities/theme/dark_theme.dart';
+import 'package:afghanistan_tourism_app/utilities/theme/light_theme.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
