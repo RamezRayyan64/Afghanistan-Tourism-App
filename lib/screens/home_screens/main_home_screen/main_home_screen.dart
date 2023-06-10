8b613c50c3ae4ee3114ae1acda5b8951b3a73fee
RamezRayyan64 <Ramez.Rayyan64@gmail.com>
2023-06-10 07:48:07 +0430

    drawer added

--- lib/screens/home_screens/main_home_screen/main_home_screen.dart
+++ lib/screens/home_screens/main_home_screen/main_home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:afghanistan_tourism_app/helper/functions.dart';
 import 'package:afghanistan_tourism_app/screens/home_screens/main_home_screen/sub_screens/drawer_screens/touristic_cities_screen/touristic_cities_screen.dart';
+import 'package:afghanistan_tourism_app/screens/home_screens/main_home_screen/sub_screens/drawer_screens/widgets/drawer_widgets/custom_drawer_widget.dart';
 import 'package:flutter/material.dart';
 
 class MainHomeScreen extends StatefulWidget {
@@ -24,6 +25,10 @@ class _MainHomeScreenState extends State<MainHomeScreen> {
       onWillPop: onWillPopScopeAction,
       child: Scaffold(
         appBar: appBarWithTitle(title: 'Afghanistan Tourism App'),
+        drawer: CustomDrawerWidget(
+          onTap: onTapDrawerItem,
+          currentDrawerIndex: currentDrawerIndex,
+        ),
         body: SingleChildScrollView(child: drawerScreens[currentDrawerIndex]),
       ),
     );
@@ -31,5 +36,17 @@ class _MainHomeScreenState extends State<MainHomeScreen> {
 
   Future<bool> onWillPopScopeAction() {
     return Future.value(false);
+  }
+
+  onTapDrawerItem(index) {
+    setState(() {
+      if (index <= 6) {
+        currentDrawerIndex = index;
+      } else if (index == 7) {
+      } else {}
+    });
+    if (index != 8) {
+      Navigator.pop(context);
+    }
   }
 }
